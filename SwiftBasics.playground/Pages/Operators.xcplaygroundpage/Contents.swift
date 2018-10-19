6605522bc40c01209b3374e491d2eba5f07b5551
Thibault <933kss@gmail.com>
2018-10-19 22:47:13 +0900

    Swift operators

---
+++ SwiftBasics.playground/Pages/Operators.xcplaygroundpage/Contents.swift
@@ -0,0 +1,22 @@
+// Binary Operators
+
+let height: Double = 12 // in ft
+let width: Double = 10 // in ft
+
+let area = height * width // Area in sq feet
+
+// 1 sq meter = 1 sq foot / 10.764
+
+let areaInMeters = area/10.764
+
+// Equals operators
+
+let string1 = "HE"
+let string2 = "he"
+let string3 = "HE!"
+let string4 = "HE"
+
+string1 == string2
+string4 == string1
+
+"a" < "b"
