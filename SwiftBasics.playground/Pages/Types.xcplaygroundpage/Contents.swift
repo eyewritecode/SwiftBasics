52ab6ef55c897d43d67ef2b3a1a10c14af258dfc
Thibault <933kss@gmail.com>
2018-10-19 19:04:36 +0900

    Swift types

---
+++ SwiftBasics.playground/Pages/Types.xcplaygroundpage/Contents.swift
@@ -0,0 +1,45 @@
+let country = "Japan"
+let city = "Osaka"
+let chuo = "Kozu"
+
+// Concatenation
+let address = country + ", " + city + " " + chuo
+
+// Interpolation
+
+let interpolatedAddress = "\(country), \(chuo)"
+
+/*
+ ----------------------
+      INTEGERS
+ ----------------------
+*/
+
+
+let favoriteProgrammingLang = "Ruby"
+let year = 2002
+
+/*
+ ----------------------
+ FLOATING NUMBERS
+ ----------------------
+ */
+
+var version = 3.0
+
+/*
+ ----------------------
+ BOOLEAN
+ ----------------------
+ */
+
+let isAwesome = true
+
+/*
+ ----------------------
+ TYPE SAFETY
+ ----------------------
+ */
+
+let bestPlayer: String = "Messi"
+let jerseyNumber: Int = 5
