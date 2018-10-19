650de6f970b70184eddf8e8cceb86181fa7e5509
Thibault <933kss@gmail.com>
2018-10-19 18:36:12 +0900

    Swift variables

---
+++ SwiftBasics.playground/Contents.swift
@@ -0,0 +1,19 @@
+// Variables
+
+var str = "Hello, playground"
+str
+str = "Hello, world!"
+//　str = 69 This returns an error because variables in SWIFT keep the initial value data type(i.e: String from LINE:3
+
+var will_smith: String
+will_smith = "Good Actor"
+will_smith
+
+// Constant
+
+let base = "THIS IS A CONSTANT"
+let 🐶 = "dog"
+
+// Naming Conventions (camelCase)
+
+let programmingLanguage = "Objective-C"
